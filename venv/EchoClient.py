6801345520a4c8a941c199d24fa9b7ec7088724d
slitherman <ericcartman2700@gmail.com>
2023-03-15 17:48:12 +0100

    changes

---
+++ venv/EchoClient.py
@@ -0,0 +1,11 @@
+from socket import *
+
+serverName = "192.168.0.199"
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input sentence: ')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From server: ', modifiedSentence.decode())
+clientSocket.close()
